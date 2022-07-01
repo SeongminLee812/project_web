2010c320337f791958b42b0309ce7a66e880fdef
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2022-07-01 09:09:53 +0900

    app추가

---
+++ edwith/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EdwithConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'edwith'
--- project_web/settings.py
+++ project_web/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project_web',
+    'kocw',
+    'kmooc',
+    'edwith',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'project_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER' : 'root',
+        'PASSWORD' : '080808',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
