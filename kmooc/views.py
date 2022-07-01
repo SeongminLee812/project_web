94806f5bf00cce387d128c55e3ab64ca7e4ace8b
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2022-07-01 18:03:35 +0900

    변경사항 커밋(원래대로 되돌려놓기)

--- kmooc/views.py
+++ kmooc/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import MoocAllList
+from django.db.models import Q # or문 사용하기 위해 import 함
 
 # Create your views here.
 def kmooc(request):
     return render(request, 'kmooc.html')
 
 def kmooc_data(request):
-    course_list = MoocAllList.objects.filter(fourth_industry_yn='y')
+    course_list = MoocAllList.objects.filter(Q(fourth_industry_yn='y')|Q(ai_sec_yn='y')).order_by('-star')[:10]
+    #or 조건문
+    print(course_list)
     return render(request, 'kmooc.html', {'MoocAllList':course_list})
     #MoocAllList(html에 전달할 값)에 위 course_list변수
 
