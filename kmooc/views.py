6353deb86990c1e2badd9d2605ac549789ed1292
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2022-07-01 14:13:41 +0900

    settings.py의 databases 및 secretkey수정 각 앱별로 models.py 입력 kmooc urls.py와 views.py 수정하여 예시 데이터 입력함

--- kmooc/views.py
+++ kmooc/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import MoocAllList
 
 # Create your views here.
 def kmooc(request):
     return render(request, 'kmooc.html')
+
+def kmooc_data(request):
+    course_list = MoocAllList.objects.filter(fourth_industry_yn='y')
+    return render(request, 'kmooc.html', {'MoocAllList':course_list})
+    #MoocAllList(html에 전달할 값)에 위 course_list변수
