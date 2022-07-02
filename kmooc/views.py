2cd002651dd2540352c1766bd90c80e184b2c4de
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2022-07-02 11:11:44 +0900

    kmooc.html에서 location.href추가 views.py에서 중복값 해결중

--- kmooc/views.py
+++ kmooc/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from .models import MoocAllList
 from django.db.models import Q # or문 사용하기 위해 import 함
+from django.db.models import Count # 중복개수 세기 위해 import 함
 
 # Create your views here.
 def kmooc(request):
     return render(request, 'kmooc.html')
 
 def kmooc_data(request):
-    course_list = MoocAllList.objects.filter(Q(fourth_industry_yn='y')|Q(ai_sec_yn='y')).order_by('-star')[:10]
-    #or 조건문
+    course_list = MoocAllList.objects.filter(Q(fourth_industry_yn='y')|Q(ai_sec_yn='y')) #.order_by('-star')[:10]    #or 조건문
+    print(course_list)
+    # 중복 있는 이름들만 구하기
+    duplicate_names = course_list.values('name').annotate(name_count=Count('name')).filter(name_count__gt=1)
+
+    print(duplicate_names)
     return render(request, 'kmooc.html', {'MoocAllList':course_list})
     #MoocAllList(html에 전달할 값)에 위 course_list변수
 
