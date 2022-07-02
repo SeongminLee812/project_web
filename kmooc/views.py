0a991ee59611e9624df0ed634665cd862be4a519
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2022-07-02 12:36:04 +0900

    kmooc.views 데이터 베이스에서 불러올 때 이름 중복값 제거하여 star 순 정렬

--- kmooc/views.py
+++ kmooc/views.py
@@ -23,41 +23,5 @@ def kmooc_data(request):
         not_dup_course_list.append(df.iloc[i])
     print(not_dup_course_list)
 
-
-    # course_list = MoocAllList.objects.distinct().value_list('name', 'fourth_industry_yn', 'ai_sec_yn', 'course_id', 'star')
-
-
-    '''
-    # print(course_list, '\n\n\n')
-    # 중복 있는 이름들만 구하기
-    duplicate_names = course_list.values('name').annotate(name_count=Count('name')).filter(name_count__gt=1)
-    # duplicate_name을 활용해서 이름 리스트를 만들기
-    dup_name_list = list()
-    for i in range(len(duplicate_names)):
-        dup_name_list.append(duplicate_names[i]['name'])
-    print(dup_name_list)
-
-    # course_list 반복문 돌리면서 이름이 duplicate_name에 있는 지 판단, 없는 것만 추가 ()
-    not_dup_course_list = list()
-    for i in course_list:
-        if i.name not in dup_name_list:
-            not_dup_course_list.append(i)
-    '''
-
-
-    # for i in course_list:
-    #     for j in range(len(duplicate_names)):
-    #         if i.name == duplicate_names[j]['name']:
-    #             dup_list.append(i)
-    # print(not_dup_list[:10], '\n\n\n')
-    # group_by로 해결해보기
-    '''
-    query = MoocAllList.objects.all().query
-    query.group_by = ['name']
-    results = QuerySet(query=query, model=MoocAllList)
-    print(results.order_by('-star')[:10])'''
-
     return render(request, 'kmooc.html', {'MoocAllList':not_dup_course_list})
     #MoocAllList(html에 전달할 값)에 위 course_list변수
-
-# test__
