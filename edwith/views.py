488eb6310fcc5857ca7e2eaf49980b32e2c6bba7
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2022-07-05 14:52:28 +0900

    정보출력중

--- edwith/views.py
+++ edwith/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import EdwithAllList
 
 # Create your views here.
 def edwith_visual(request):
-    return render(request, 'edwith.html')
+    course_list01 = EdwithAllList.objects.all().order_by('-like_num')[:10]
+    course_list02 = EdwithAllList.objects.all().order_by('-like_num')[:10]
+
+    return render(request, 'edwith.html', {'EdwithAllList': course_list01})
 
 # GIT TEST ID
